f7e944d8932979c758ae1e03c77f3bfda6adafd6
Juan van der Breggen <juanb@redhat.com>
2026-02-28 22:35:30 +0000

    Cleanup of old entries

--- mamerom/mame_arcade.sh
+++ mamerom/mame_arcade.sh
@@ -3,7 +3,7 @@
 # and replace "mamerom.zip" in this retro_arcade.sh and in the Containerfile with the filename of the zip ROM file.
 
 # Uncomment if you are running this on a MAME make OS.
-#$HOME/mame/mame /opt/mame/roms/mamerom.zip
+# /opt/mame/mame /opt/mame/roms/mamerom.zip
 
 # Comment if you are running this on a Mame make OS. Make sure to place the MAME ROM in /opt/mame/roms and update the filename in the command below.
 /usr/bin/flatpak run org.mamedev.MAME /opt/mame/roms/mamerom.zip
