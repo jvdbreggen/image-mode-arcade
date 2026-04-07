ad4a0289cacf6f8f71d17bef7fe6355c576700e5
Juan <juan@breggen.net>
2026-04-07 12:50:03 +0100

    Add option to inhibit gnome permissions

--- mamerom/mame_arcade.sh
+++ mamerom/mame_arcade.sh
@@ -2,6 +2,9 @@
 # Note: You need to download a MAME ROM and place it in the same directory as the Containerfile
 # and replace "mamerom.zip" in this retro_arcade.sh and in the Containerfile with the filename of the zip ROM file.
 
+# Needed to prevent gnome from asking permissions - Uncomment if you are using this container image to deploy a VM using bootc-image-builder
+# /usr/bin/flatpak permission-set gnome shortcuts-inhibitor org.mamedev.MAME.desktop GRANTED
+
 # Uncomment if you are running this on a MAME make OS.
 # /opt/mame/mame /opt/mame/roms/mamerom.zip
 
